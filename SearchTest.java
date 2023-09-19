9635ab8544d44c37c20c5e03c1f8e0a7035791ad
Иван <87786667+Venues911@users.noreply.github.com>
2023-09-19 13:07:44 +0300

    Update SearchTest.java

--- SearchTest.java
+++ SearchTest.java
@@ -9,6 +9,7 @@ vpublic class SearchTests {
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
+     !!!
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 }
