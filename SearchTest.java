c8c532c4caf53f2b1ffa508a80cdbb18a0d2554f
Иван <87786667+Venues911@users.noreply.github.com>
2023-09-19 13:05:45 +0300

    Update SearchTest.java

--- SearchTest.java
+++ SearchTest.java
@@ -4,13 +4,11 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SearchTests {
+vpublic class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.booble.com/");
-        $("[notname=q]").setValue("SELENIUM").pressEnter();
-        $("[NotId=search]").shouldHave(text("https://SEleNium.org"));
-
-        !
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 }
